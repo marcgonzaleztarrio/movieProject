a2ad68cd4ec6f833144bf6c9919860e44c52c726
Marc Gonzalez <marcgonzaleztarrio@gmail.com>
2023-05-12 10:32:29 +0200

    nice

--- app.js
+++ app.js
@@ -22,19 +22,12 @@ let getMovies = () => {
       console.log(data.results);
       results.innerHTML = `
       <div class="info">
-          <h1>${data.results[0].original_title}</h1>
-          <img src="https://image.tmdb.org/t/p/w300${data.results[0].poster_path}">
-        <p>${data.results[0].overview}</p>
+      <p id="voteAverage">${data.results[0].vote_average}</p>
+          <h1 id="movieTitle">${data.results[0].original_title}</h1>
+          <img src="https://image.tmdb.org/t/p/w200${data.results[0].poster_path}">
+        <p id="movieOverview">${data.results[0].overview}</p>
+        <p id="releaseDate">${data.results[0].release_date}</p>
           </div>
       `;
-      //   if (data.response == "True") {
-      //     results.innerHTML = `
-      //         <div class="info">
-      //             <img src=${data.poster_path}>
-      //         </div>
-      //         `;
-      //   } else {
-      //     console.log("lokooooo");
-      //   }
     });
 };
