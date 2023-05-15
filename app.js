63f231fdce975f8281dad3ddb5149e74c788f5ef
TumarreTorre <marcgonzaleztarrio@gmail.com>
2023-05-15 13:11:52 +0200

    arrowsAndRemove

--- app.js
+++ app.js
@@ -10,138 +10,79 @@ console.log(input.value);
 input.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
     event.preventDefault();
+    currentPage = 0;
     getMovies();
   }
 });
 
+buttonMovie.addEventListener("click", function (event) {
+  event.preventDefault();
+  currentPage = 0;
+  getMovies();
+});
+
+let currentPage = 0;
+
 let getMovies = () => {
-  let movieName = input.value;
   let url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${input.value}`;
 
-  if (movieName.length <= 0) {
-    results.innerHTML = `<h3 class="msg">Please enter a movie name</h3>`;
+  if (input.value.length <= 0) {
+    results.innerHTML = `<h3 class="info noData">Please enter a movie name</h3>`;
   } else {
   }
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
-      let flechaD = document.getElementsByClassName("flechaD");
-
-      results.innerHTML = `
+      console.log(data.results);
+      data.results.length === 0
+        ? (results.innerHTML = `<h3 class="info noData">Please enter a movie name</h3>`)
+        : (results.innerHTML = `
       <div class="info">
-      <a class="flechaD" onclick="restarPelicula()">⪻</a>
-      <a class="flechaIz" onclick="sumarPelicula()">⪼</a>
-          <h1 id="movieTitle">${data.results[0].original_title}</h1>
-            <img src="https://image.tmdb.org/t/p/w200${data.results[0].poster_path}">
-            <p id="voteAverage">${data.results[0].vote_average}/10</p>
-        <p id="movieOverview">${data.results[0].overview}</p>
-        <p id="releaseDate">${data.results[0].release_date}</p>
+      <a class="flechaD" id="flechaD">⪼</a>
+      <a class="flechaIz" id="flechaIz">⪻</a>
+          <h1 id="movieTitle">${data.results[currentPage].title}</h1>
+            <img id="image404" src="https://image.tmdb.org/t/p/w200${data.results[currentPage].poster_path}">
+            <p id="voteAverage">${data.results[currentPage].vote_average}/10</p>
+        <p id="movieOverview">${data.results[currentPage].overview}</p>
+        <p id="releaseDate">${data.results[currentPage].release_date}</p>
           </div>
-      `;
+      `);
+      removeArrows(data);
+      arrowsChangeMovie();
+      changeImg(data);
     });
 };
 
-let restarPelicula = (data) => {
-  console.log("Restar Pelicula");
-  console.log("loko2");
-  let info = document.querySelector(".info");
-  info.classList.add("hidden");
-  results2.innerHTML = `
-  <div class="info2">
-  <a class="flechaD">⪻</a>
-  <a class="flechaIz">⪼</a>
-      <h1 id="movieTitle">${data.results[0 + 1].original_title}</h1>
-        <img src="https://image.tmdb.org/t/p/w200${
-          data.results[0 + 1].poster_path
-        }">
-        <p id="voteAverage">${data.results[0 + 1].vote_average}/10</p>
-    <p id="movieOverview">${data.results[0 + 1].overview}</p>
-    <p id="releaseDate">${data.results[0 + 1].release_date}</p>
-      </div>
-  `;
+let removeArrows = (data) => {
+  let flechaIz = document.getElementById("flechaIz");
+  let flechaD = document.getElementById("flechaD");
+
+  currentPage + 1 === data.results.length
+    ? flechaD.classList.add("hidden")
+    : flechaD.classList.remove("hidden");
+
+  currentPage === 0
+    ? flechaIz.classList.add("hidden")
+    : flechaIz.classList.remove("hidden");
 };
 
-let sumarPelicula = () => {
-  let url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${input.value}`;
+let arrowsChangeMovie = () => {
+  let flechaD = document.getElementById("flechaD");
+  let flechaIz = document.getElementById("flechaIz");
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
+  flechaD.addEventListener("click", function () {
+    currentPage++;
+    getMovies();
+  });
 
-      console.log("this is data page " + (data.page + 1));
-
-      let info = document.querySelector(".info");
-      info.classList.add("hidden");
-      results2.innerHTML = `
-  <div class="info2">
-  <a class="flechaD">⪻</a>
-  <a class="flechaIz" onclick="sumarPelicula()">⪼</a>
-      <h1 id="movieTitle">${data.results[+1].original_title}</h1>
-        <img src="https://image.tmdb.org/t/p/w200${
-          data.results[+1].poster_path
-        }">
-        <p id="voteAverage">${data.results[+1].vote_average}/10</p>
-    <p id="movieOverview">${data.results[+1].overview}</p>
-    <p id="releaseDate">${data.results[+1].release_date}</p>
-      </div>
-  `;
-    });
+  flechaIz.addEventListener("click", function () {
+    currentPage--;
+    getMovies();
+  });
 };
 
-// ⪻
-
-// let getNextMovie = (data) => {
-//   console.log("loko2");
-//   let info = document.querySelector(".info");
-//   info.classList.add("hidden");
-//   results.innerHTML = `
-//   <div class="info">
-//   <a class="flechaD">⪻</a>
-//   <a class="flechaIz">⪼</a>
-//       <h1 id="movieTitle">${data.results[0 + 1].original_title}</h1>
-//         <img src="https://image.tmdb.org/t/p/w200${
-//           data.results[0 + 1].poster_path
-//         }">
-//         <p id="voteAverage">${data.results[0 + 1].vote_average}/10</p>
-//     <p id="movieOverview">${data.results[0 + 1].overview}</p>
-//     <p id="releaseDate">${data.results[0 + 1].release_date}</p>
-//       </div>
-//   `;
-// };
-
-// let getMovies = () => {
-//   let movieName = input.value;
-//   let url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${movieName}`;
-
-//   fetch(url)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       console.log(data.results);
-
-//       data.results.length === 0
-//         ? (results.innerHTML = ` <div class="info noData"><h3>Please Enter Movie Name</h3></div>`)
-//         : data.results.length >= 2
-//         ? (results.innerHTML = `
-//       <div class="info">
-//       <a class="flechaD">⪻</a>
-//       <a class="flechaIz">⪼</a>
-//           <h1 id="movieTitle">${data.results[0].original_title}</h1>
-//             <img src="https://image.tmdb.org/t/p/w200${data.results[0].poster_path}">
-//             <p id="voteAverage">${data.results[0].vote_average}/10</p>
-//         <p id="movieOverview">${data.results[0].overview}</p>
-//         <p id="releaseDate">${data.results[0].release_date}</p>
-//           </div>
-//       `)
-//         : (results.innerHTML = `
-//         <div class="info">
-//             <h1 id="movieTitle">${data.results[0].original_title}</h1>
-//               <img src="https://image.tmdb.org/t/p/w200${data.results[0].poster_path}">
-//               <p id="voteAverage">${data.results[0].vote_average}/10</p>
-//           <p id="movieOverview">${data.results[0].overview}</p>
-//           <p id="releaseDate">${data.results[0].release_date}</p>
-//             </div>
-//         `);
-//     });
-// };
+let changeImg = (data) => {
+  data.results[currentPage].poster_path === null
+    ? (document.getElementById("image404").src = "./NotFoundMovieNice.png")
+    : data.results[currentPage].poster_path;
+};
